0b6cd50908b9daa1caf0268bd11ec802990282e8
Khris Punzalan <khrispunzalan@gmail.com>
2019-07-09 21:10:58 +0000

    Done.

---
+++ lib/student.rb
@@ -0,0 +1,13 @@
+class Student
+
+  def hello
+    greeting = "Hey there! I'm so excited to learn stuff."
+    puts greeting
+  end
+
+  def raise_hand
+    phrase = "Pick me!"
+    puts phrase
+  end
+
+end
