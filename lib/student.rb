dcdd7a2fd16774924b240a2b6d18c59a2d206416
Hillary Spitzer <hspitzer2@gmail.com>
2019-07-23 14:10:21 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,16 +1,10 @@
 class Student
-  attr_accessor :hello, :raise_hand
-
-  def initialize
-    @hello = hello
-    @raise_hand = raise_hand
-  end
 
   def hello
-    "Hey there!"
+    puts "Hey there! I'm so excited to learn stuff."
   end
 
   def raise_hand
-    puts "pick me"
+    puts "Pick me!"
   end
 end
